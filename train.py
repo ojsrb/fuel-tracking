5424d6b9087a83eaf239f79fe7e46201cbc2cb8f
Owen <owenschmidt2010@gmail.com>
2026-03-07 11:17:01 -0600

    add dataset.zip

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
 
-model.train(data="dataset/data.yaml", epochs=256, imgsz=640, device="mps")
+model.train(data="dataset/data.yaml", epochs=256, imgsz=640, device="cpu")
 
 model.save("fuel.pt")
