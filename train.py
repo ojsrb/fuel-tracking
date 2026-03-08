b6fb77b5f67cbcadf9d3c2d5705abf2d4365874e
Owen Schmidt <66569940+ojsrb@users.noreply.github.com>
2026-03-07 18:21:14 -0600

    Change device parameter from 'cpu' to '0' in training

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")
 
-model.train(data="dataset/data.yaml", epochs=256, imgsz=640, device="cpu")
+model.train(data="dataset/data.yaml", epochs=256, imgsz=640, device=0)
 
 model.save("fuel.pt")
